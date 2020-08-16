424320405e8046a3a93834835479dc512fcea586
yehric2018 <eric.yehj@outlook.com>
2020-08-15 23:14:12 -0700

    add roommates

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify
+from firebase import firebase
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
+firebase = firebase.FirebaseApplication("https://housing-roommate-finder-286520.firebaseio.com/", None)
 
 @app.route('/')
 def get_html():
@@ -10,3 +12,49 @@ def get_html():
 @app.route('/time')
 def get_current_time():
 	return {'time': time.time()}
+
+@app.route('/add_listing')
+def add_listing():
+	description = request.args.get("desc", type=str)
+	major = request.args.get("length", type=int)
+	location = request.args.get("loc", type=str)
+	price = request.args.get("price", type=int)
+	squareFootage = request.args.get("area", type=int)
+	title = request.args.get("title", type=str)
+
+	listing = {
+		"description": "Great amenities! You will be sharing a room with one other person!",
+		"length": 12,
+		"location": "4444 444th St Seattle, WA",
+		"price": 800,
+		"squareFootage": 700,
+		"title": "Looking for another roommate!"
+	}
+
+	result = firebase.post('/listings', listing, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+	return result
+
+@app.route('/add_roommate')
+def add_roommate():
+	name = request.args.get('name', type=str)
+	major = request.args.get('major', type=str)
+	profileImage = request.args.get('img', type=str)
+	sleepStart = request.args.get('sleepStart', type=str)
+	sleepEnd = request.args.get('sleepEnd', type=str)
+	habits = request.args.get('habits', type=str).split(",")
+	room_id = request.args.get('room_id', type=str)
+	roommate = {
+		"name": name,
+		"major": major,
+		"profileImage": profileImage,
+		"sleepSchedule": {"start": sleepStart, "end": sleepEnd},
+		"habits": habits,
+		"listing_id": room_id
+	}
+	result = firebase.post("/roommates", roommate, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+	return result
+
+@app.route('/housing', methods=["GET"])
+def get_housing_info():
+	result = firebase.get('/listings', None)
+	return str(result)
