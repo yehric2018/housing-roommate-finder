959eabde231faf178187e53b3b99a83437fb77e0
yehric2018 <eric.yehj@outlook.com>
2020-08-16 00:12:14 -0700

    material

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import AddListingPage from './AddListingPage/AddListingPage';
+import LandingPage from './landingPage/landingPage';
 
 function App() {
   return (
       <div className="App">
-          <AddListingPage />
+          <LandingPage />
     </div>
   );
 }
